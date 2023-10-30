d515af9667140f4ce8c0f06e532ea6bc309a9adc
thientm27 <denkhotieu1@gmail.com>
2023-10-30 19:38:36 +0700

    init scripts

---
+++ Assets/Scripts/GameScene/GameModel.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace GameScene
+{
+    public class GameModel: MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameScene/GameView.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace GameScene
+{
+    public class GameView  : MonoBehaviour
+    {
+        
+    }
+}
